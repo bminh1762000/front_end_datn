35f913ae332954f3c4a3be83ee227043e11e19a9
bminh1762000 <vukhachungyen@gmail.com>
2023-04-17 02:00:18 +0700

    feat: add commit lint to project

--- src/container/CollectionContainer.tsx
+++ src/container/CollectionContainer.tsx
@@ -1,18 +1,15 @@
-import { connect } from "react-redux";
-import { compose } from "redux";
-import { createStructuredSelector } from "reselect";
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
 
-import { selectIsCollectionsLoaded } from "../redux/shop/shop.selector";
-import WithSpinner from "../components/WithSpinner";
-import CollectionPage from "../pages/Collection";
+import { selectIsCollectionsLoaded } from '../redux/shop/shop.selector';
+import WithSpinner from '../components/WithSpinner';
+import CollectionPage from '../pages/Collection';
 
 const mapStateToProps = createStructuredSelector({
-  isLoading: selectIsCollectionsLoaded,
+    isLoading: selectIsCollectionsLoaded,
 });
 
-const CollectionPageContainer = compose(
-  connect(mapStateToProps),
-  WithSpinner
-)(CollectionPage);
+const CollectionPageContainer = compose(connect(mapStateToProps), WithSpinner)(CollectionPage);
 
 export default CollectionPageContainer;
